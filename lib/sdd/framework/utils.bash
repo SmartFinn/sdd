02a9421700c5aeada1dbe774ba50dda997b6e71a
Sergei Eremenko <finalitik@gmail.com>
2020-02-26 13:42:38 +0200

    Update code style

--- lib/sdd/framework/utils.bash
+++ lib/sdd/framework/utils.bash
@@ -1,11 +1,9 @@
 # Utility functions for sdd framework
 
-FRAMEWORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+FRAMEWORKDIR="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 
 utils_usage() {
-    while IFS= read -r line; do
-        printf '%s\n' "$line"
-    done <<END_OF_HELP_TEXT
+    cat <<END_OF_HELP_TEXT
 Usage: sdd [OPTIONS] COMMAND [APP [APP...]]
 
 A framework to manage installation of apps from web sources for non-root users
@@ -34,350 +32,305 @@ END_OF_HELP_TEXT
 utils_version() {
     if [ ! -e "$SDD_DATA_DIR"/apps/installed ]; then
         printf 'Cannot find %s.\n' "$SDD_DATA_DIR"/apps/installed >&2
-        return 1;
+        return 1
     fi
 
-    tac "$SDD_DATA_DIR"/apps/installed | grep -m 1 '^sdd=' | cut -d '=' -f2
+    tac "$SDD_DATA_DIR"/apps/installed | awk -F= '/^sddj/ {print $2; exit}'
 }
 
-_validate_apps() {
-    local return_code=0
-    local appfilepath
-    local apps=()
-    for app in "$@"; do
-        local nr_misses=0
+_get_appfilepath() {
+    local app_name="$1"
 
-        for dir in "$FRAMEWORKDIR/../apps/user" "$HOME/.config/sdd/apps"; do
-            appfilepath="$dir/$app"
-
-            # Check whether app available
-            if [ ! -f "$appfilepath" ]; then
-                ((nr_misses++))
-            fi
-        done
-
-        if [ $nr_misses -eq 2 ]; then
-            printf 'App "%s" could not be found.\n' "$app" >&2
-            return_code=2
-        else
-            apps+=($app)
+    for dir in "$HOME/.config/sdd/apps" "$FRAMEWORKDIR/../apps/user"; do
+        if [ -f "$dir/$app_name" ]; then
+            echo "$dir/$app_name"
+            return 0
         fi
     done
 
-    echo ${apps[@]}
-    return $return_code
+    return 1
+}
+
+_validate_apps() {
+    local arg app_name
+
+    for arg in "$@"; do
+        # Extract app name from the argument
+        app_name="${arg%%=*}"
+
+        # Check whether app available
+        if ! _get_appfilepath "$app_name" >/dev/null; then
+            printf 'App "%s" could not be found.\n' "$app_name" >&2
+            return 1
+        fi
+    done
+
+    return 0
 }
 
 utils_install() {
-    local return_code=0
+    local -i return_code=0
+    local arg app_name app_version
+    local stdoutlog stderrlog
 
     # Install one or more apps
-    if [ $# -eq 0 ]; then
+    if [ -z "$1" ]; then
         printf 'Specify at least one app to install.\n' >&2
         return 1
     fi
 
-    # Extract only app names from arguments for validation
-    local all_apps=()
+    # Abort if one of the specified apps doesn't exist
+    _validate_apps "$@" || return 1
+
     for arg in "$@"; do
-        all_apps+=($(echo $arg | cut -d"=" -f1))
-    done
+        # Extract app name from the argument
+        app_name="${arg%%=*}"
 
-    local apps=()
-    apps=($(_validate_apps "${all_apps[@]}"))
-    return_code=$?
+        # Extract app version from the argument
+        app_version=""
+        if [[ $arg == *"="* ]]; then
+            app_version="${arg##*=}"
+        fi
 
-    for app in "${apps[@]}"; do
-        local stdoutlog=/tmp/sdd-install-$app.stdout
-        local stderrlog=/tmp/sdd-install-$app.stderr
+        stdoutlog="/tmp/sdd-install-$app_name.stdout"
+        stderrlog="/tmp/sdd-install-$app_name.stderr"
 
         # Execute installation; tee stdout/stderr to files, see
         # https://stackoverflow.com/a/53051506
-        local rc
-        { _utils_install_one "$app" "$@"> >(tee $stdoutlog ); } 2> >(tee $stderrlog >&2 )
-        rc=$?
+        { _utils_install_one "$app_name" "$app_version" > >(tee "$stdoutlog"); } 2> >(tee "$stderrlog" >&2)
+        return_code="$?"
 
-        if [ $rc -ne 0 ]; then
-            printf 'Error installing "%s". See above and %s.\n' "$app" "$stderrlog" > >(tee -a $stderrlog >&2 )
+        if [[ $return_code -ne 0 ]]; then
+            printf 'Error installing "%s". See above and %s.\n' "$app_name" "$stderrlog" > >(tee -a "$stderrlog" >&2)
 
-            ((return_code+=rc))
+            return "$return_code"
         fi
     done
 
-    return $return_code
+    return 0
 }
 
 utils_uninstall() {
-    local return_code=0
+    local -i return_code=0
+    local arg app_name
+    local stdoutlog stderrlog
 
     # Uninstall one or more apps
-    if [ $# -eq 0 ]; then
+    if [ -z "$1" ]; then
         printf 'Specify at least one app to uninstall.\n' >&2
         return 1
     fi
 
-    local apps=()
-    apps=($(_validate_apps "$@"))
-    return_code=$?
+    # Abort if one of the specified apps doesn't exist
+    _validate_apps "$@" || return 1
 
-    for app in "${apps[@]}"; do
-        local stdoutlog=/tmp/sdd-uninstall-$app.stdout
-        local stderrlog=/tmp/sdd-uninstall-$app.stderr
+    for arg in "$@"; do
+        # Extract app name from the argument
+        app_name="${arg%%=*}"
+
+        stdoutlog=/tmp/sdd-uninstall-$app_name.stdout
+        stderrlog=/tmp/sdd-uninstall-$app_name.stderr
 
         # Execute uninstallation; tee stdout/stderr to files, see
         # https://stackoverflow.com/a/53051506
-        local rc
-        { _utils_uninstall_one "$app" > >(tee $stdoutlog ); } 2> >(tee $stderrlog >&2 )
-        rc=$?
+        { _utils_uninstall_one "$app_name" > >(tee "$stdoutlog"); } 2> >(tee "$stderrlog" >&2) || {
+            return_code="$?"
+            printf 'Error uninstalling "%s". See above and %s.\n' "$app_name" "$stderrlog" > >(tee -a "$stderrlog" >&2)
 
-        if [ $rc -ne 0 ]; then
-            printf 'Error uninstalling "%s". See above and %s.\n' "$app" "$stderrlog" > >(tee -a $stderrlog >&2 )
-
-            ((return_code+=rc))
-        fi
+            return "$return_code"
+        }
     done
 
-    return $return_code
+    return 0
 }
 
 _utils_install_one() {
-    local app="$1"
     # The remaining arguments apps to be installed, as passed into utils_install,
     # i.e. possibly with versions specified
-    shift
+    local app_name="$1"
+    local app_version="$2"
 
-    if [ -f "$HOME/.config/sdd/apps/$app" ]; then
-        printf 'Custom installation for "%s" found.\n' "$app"
+    if [ -f "$HOME/.config/sdd/apps/$app_name" ]; then
+        printf 'Custom installation for "%s" found.\n' "$app_name"
     fi
 
-    local version
-    # Try to parse version from arguments
-    for arg in "$@"; do
-        if [[ $arg = $app=* ]]; then
-            version=$(echo $arg | cut -d"=" -f2)
-            printf 'Specified version: %s\n' $version
-            break
-        fi
-    done
-
-    # If version not specified, try to read it from the app management files.
-    if [ -z $version ]; then
-        version=$(_utils_app_version_from_files "$app")
-
-        if [[ -n $version ]]; then
-            printf 'Latest version available: %s\n' $version
-        fi
+    if [[ -n $app_version ]]; then
+        printf 'Specified version: %s\n' "$app_version"
+    else
+        # If version not specified, try to read it from the app management files.
+        app_version=$(_utils_app_version_from_files "$app_name") || {
+            printf 'Latest version available: %s\n' "$app_version"
+        }
     fi
 
-    local success=False
+    # Cleanup current scope
+    unset -f sdd_install 2>/dev/null || true
 
-    for dir in "$FRAMEWORKDIR/../apps/user" "$HOME/.config/sdd/apps"; do
-        appfilepath="$dir/$app"
+    # Source app management file
+    source "$(_get_appfilepath "$app_name")" || return 1
 
-        if [ ! -f "$appfilepath" ]; then
-            continue
-        fi
+    sdd_install "$app_version"
 
-        # Cleanup current scope
-        unset -f sdd_install 2> /dev/null || true
-        # Source app management file
-        source "$appfilepath"
-        sdd_install $version
-
-        if [ $? -eq 0 ] && [ $success = False ]; then
-            printf 'Installed "%s".\n' "$app"
-            success=True
-
-            # Record installed app and version (can be empty)
-            echo $app=$version >> "$SDD_DATA_DIR"/apps/installed
-        fi
-    done
-
-    if [ $success = False ]; then
-        return_code=4
+    # Remove previous records
+    if [ -f "$SDD_DATA_DIR"/apps/installed ]; then
+        sed -i "/^$app_name=/d" "$SDD_DATA_DIR"/apps/installed
     fi
 
-    return $return_code
+    # Record installed app and version (can be empty)
+    echo "$app_name=$app_version" >> "$SDD_DATA_DIR"/apps/installed
+
+    printf 'Installed "%s".\n' "$app_name"
+    return 0
 }
 
 _utils_uninstall_one() {
-    local app=$1
-    if [ -f "$HOME/.config/sdd/apps/$app" ]; then
-        printf 'Custom uninstallation for "%s" found.\n' "$app"
+    local app_name="$1"
+
+    if [ -f "$HOME/.config/sdd/apps/$app_name" ]; then
+        printf 'Custom uninstallation for "%s" found.\n' "$app_name"
     fi
 
-    local success=False
+    # Cleanup current scope
+    unset -f sdd_uninstall 2>/dev/null || true
 
-    local appfilepath
-    for dir in "$FRAMEWORKDIR/../apps/user" "$HOME/.config/sdd/apps"; do
-        appfilepath="$dir/$app"
+    # Source app management file
+    source "$(_get_appfilepath "$app_name")" || return 1
 
-        if [ ! -f "$appfilepath" ]; then
-            continue
-        fi
+    sdd_uninstall
 
-        # Cleanup current scope
-        unset -f sdd_uninstall 2> /dev/null || true
-        # Source app management file
-        source "$appfilepath"
-        sdd_uninstall
-
-        if [ $? -eq 0 ] && [ $success = False ]; then
-            printf 'Uninstalled "%s".\n' "$app"
-            success=True
-
-            if [ -f "$SDD_DATA_DIR"/apps/installed ]; then
-                # Remove app install records
-                sed -i "/^$app/d" "$SDD_DATA_DIR"/apps/installed
-            fi
-        fi
-    done
-
-    if [ $success = False ]; then
-        return_code=4
+    if [ -f "$SDD_DATA_DIR"/apps/installed ]; then
+        # Remove app install records
+        sed -i "/^$app_name=/d" "$SDD_DATA_DIR"/apps/installed
     fi
 
-    return $return_code
+    printf 'Uninstalled "%s".\n' "$app_name"
+    return 0
 }
 
 _utils_upgrade_one() {
-    local app="$1"
-    # The remaining arguments apps to be installed, as passed into utils_upgrade,
-    # i.e. possibly with versions specified
-    shift
+    local app_name="$1"
+    local app_version="$2"
 
-    local return_code
+    _utils_uninstall_one "$app_name"
+    _utils_install_one "$app_name" "$app_version"
 
-    _utils_uninstall_one "$app"
-    return_code=$?
-
-    if [ $return_code -eq 0 ]; then
-        _utils_install_one "$app" "$@"
-        return_code=$?
-    fi
-
-    if [ $return_code -eq 0 ]; then
-        printf 'Upgraded "%s".\n' "$app"
-    fi
-
-    return $return_code
+    printf 'Upgraded "%s".\n' "$app_name"
+    return 0
 }
 
 utils_upgrade() {
-    local return_code=0
+    local -i return_code=0
+    local arg app_name app_version
+    local stdoutlog stderrlog
 
-    if [ $# -eq 0 ]; then
+    if [ -z "$1" ]; then
         printf 'Specify at least one app to upgrade.\n' >&2
         return 1
     fi
 
-    # Extract only app names from arguments for validation
-    local all_apps=()
+    # Abort if one of the specified apps doesn't exist
+    _validate_apps "$@" || return 1
+
     for arg in "$@"; do
-        all_apps+=($(echo $arg | cut -d"=" -f1))
-    done
+        # Extract app name from the argument
+        app_name="${arg%%=*}"
 
-    local apps=()
-    apps=($(_validate_apps "${all_apps[@]}"))
-    return_code=$?
-
-    local app
-    for arg in "$@"; do
-        app=$(echo $arg | cut -d"=" -f1)
-
-        local stdoutlog=/tmp/sdd-upgrade-$app.stdout
-        local stderrlog=/tmp/sdd-upgrade-$app.stderr
-
-        local rc
-        { _utils_upgrade_one "$app" "$@" > >(tee $stdoutlog ); } 2> >(tee $stderrlog >&2 )
-        rc=$?
-
-        if [ $rc -ne 0 ]; then
-            printf 'Error upgrading "%s". See above and %s.\n' "$app" "$stderrlog" > >(tee -a $stderrlog >&2 )
-
-            ((return_code+=rc))
+        # Extract app version from the argument
+        app_version=""
+        if [[ $arg == *"="* ]]; then
+            app_version="${arg##*=}"
         fi
+
+        stdoutlog=/tmp/sdd-upgrade-$app_name.stdout
+        stderrlog=/tmp/sdd-upgrade-$app_name.stderr
+
+        { _utils_upgrade_one "$app_name" "$app_version" > >(tee "$stdoutlog"); } 2> >(tee "$stderrlog" >&2) || {
+            return_code="$?"
+            printf 'Error upgrading "%s". See above and %s.\n' "$app_name" "$stderrlog" > >(tee -a "$stderrlog" >&2)
+
+            return "$return_code"
+        }
     done
 
-    return $return_code
+    return 0
 }
 
 _utils_app_version_from_files() {
     # Determine relevant version of app from app management files by executing
     # the sdd_fetch_latest_version() functions.
     # The custom definition takes precedence over the built-in one.
-    local app="$1"
-    local appfilepath
-    local version
+    local app_name="$1"
 
-    for dir in "$FRAMEWORKDIR/../apps/user" "$HOME/.config/sdd/apps"; do
-        appfilepath="$dir/$app"
+    unset -f sdd_fetch_latest_version 2>/dev/null || true
+    source "$(_get_appfilepath "$app_name")"
 
-        if [ ! -f "$appfilepath" ]; then
-            continue
-        fi
+    sdd_fetch_latest_version 2>/dev/null
 
-        unset -f sdd_fetch_latest_version 2> /dev/null || true
-        source "$appfilepath"
-
-        local version_from_file
-        version_from_file=$(sdd_fetch_latest_version 2>/dev/null)
-        if [ $? -eq 0 ]; then
-            version=$version_from_file
-        fi
-    done
-
-    echo "$version"
+    return 0
 }
 
 utils_list() {
-    local option=$1
+    local option="$1"
 
-    if [ "$option" = "--installed" ]; then
-        if [ -f "$SDD_DATA_DIR"/apps/installed ]; then
-            # List apps installed most recently by filtering unique app names first
-            for app in $(cut -d"=" -f1 "$SDD_DATA_DIR"/apps/installed | sort | uniq | xargs); do
-                grep "^$app=" "$SDD_DATA_DIR"/apps/installed | tail -n1
-            done
-        fi
-    elif [ "$option" = "--available" ]; then
-        ls -1 "$FRAMEWORKDIR/../apps/user"
-    elif [ "$option" = "--upgradable" ]; then
-        local name
-        local installed_version
-        local newest_version
-
-        for name_version in $(utils_list --installed | xargs); do
-            name=$(echo "$name_version" | cut -d"=" -f1)
-            installed_version=$(echo "$name_version" | cut -d"=" -f2)
-            newest_version=$(_utils_app_version_from_files "$name")
-
-            if [[ "$installed_version" != "$newest_version" ]]; then
-                printf '%s (%s -> %s)\n' "$name" "$installed_version" "$newest_version"
+    case "$option" in
+        --installed|-i)
+            if [ -f "$SDD_DATA_DIR"/apps/installed ]; then
+                # List apps installed most recently by filtering unique app names first
+                tac "$SDD_DATA_DIR"/apps/installed | sort -t= -k1,1 -u
             fi
-        done
-    else
-        printf 'Unknown option "%s".\n' "$option" >&2
-        return 1
-    fi
+            ;;
+        --available|-a)
+            ls -1 "$FRAMEWORKDIR/../apps/user"
+            ;;
+        --upgradable|-u)
+            local name
+            local installed_version
+            local newest_version
+
+            utils_list --installed | while IFS='=' read -r name installed_version; do
+                newest_version=$(_utils_app_version_from_files "$name")
+
+                if [[ "$installed_version" != "$newest_version" ]]; then
+                    printf '%s (%s -> %s)\n' "$name" "$installed_version" "$newest_version"
+                fi
+            done
+            ;;
+        *)
+            printf 'Unknown option "%s".\n' "$option" >&2
+            return 1
+            ;;
+    esac
 }
 
 _tag_name_of_latest_github_release() {
     # Fetch tag name of latest release on GitHub
     local github_user=$1
     local repo_name=$2
-    wget -qO- https://api.github.com/repos/$github_user/$repo_name/releases/latest | grep tag_name | awk '{ print $2; }' | sed 's/[",]//g'
+
+    # Get tag name from URL redirection to bypass GitHub API limits
+    awk -F'[ /]' -v err=1 '/Location:/ {err=0; print $(NF-1);} END {exit(err);}' < <(
+        wget -o- --max-redirect 0 "https://github.com/$github_user/$repo_name/releases/latest"
+    )
 }
 
 _sha_of_github_master() {
     # Fetch SHA of latest commit on GitHub master branch
     local github_user=$1
     local repo_name=$2
-    wget -qO- https://api.github.com/repos/$github_user/$repo_name/commits/master | grep -m1 sha | awk '{ print $2; }' | sed 's/[",]//g'
+    local branch=${3:-master}
+
+    # Get latest commit hash from atom feed to bypass GitHub API limits
+    wget -qO- "https://github.com/$github_user/$repo_name/commits/$branch.atom" |
+        awk -F'[/<>]' '/<id>tag:github.com,2008:Grit::Commit/ {i[n++] = $(NF-3);}
+        END {print i[0];}'
 }
 
 _name_of_latest_github_tag() {
     local github_user=$1
     local repo_name=$2
-    wget -qO- https://api.github.com/repos/$github_user/$repo_name/tags | grep -m1 name | awk '{ print $2; }' | sed 's/[",]//g'
+
+    # Get latest tag from atom feed to bypass GitHub API limits
+    wget -qO- "https://github.com/$github_user/$repo_name/tags.atom" |
+        awk -F'[/<>]' '/<id>tag:github.com,2008:Repository/ {i[n++] = $(NF-3);}
+            END {print i[0];}'
 }
